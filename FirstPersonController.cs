e909f4a44d539ea42cc962a75470d14fab2ab0a6
Paige Hinkle <paigehinkle@utexas.edu>
2016-02-02 23:33:47 -0600

    fpc updates

--- FirstPersonController.cs
+++ FirstPersonController.cs
@@ -1,52 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof(CharacterController))]
-public class FirstPersonController : MonoBehaviour {
+public class FirstPersonController : MyController {
 
-	public float movementSpeed = 10.0f;
-	public float mouseSensitivity = 3.0f;
-	public float jumpSpeed = 7.0f;
-
-	float verticalRotation = 0;
-	public float upDownRange = 60.0f;
-
-	float gravity = Physics.gravity.y / 2;
-	float verticalVelocity = 0;
-
-	CharacterController characterController;
-
-	// Use this for initialization
-	void Start () {
-		//Screen.lockCursor = true;
-		characterController = GetComponent<CharacterController> ();
+	public void RotateCharacter (float verticalRotation) {
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		// Rotation
-		float rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensitivity;
-		transform.Rotate (0, rotLeftRight, 0);
 
-		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
-		verticalRotation = Mathf.Clamp (verticalRotation, -upDownRange, upDownRange);
-
-		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-
-		// Movement
-		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
-		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
-
-		// Could add in double jump here
-		if (Input.GetButtonDown ("Jump") && characterController.isGrounded) {
-			verticalVelocity = jumpSpeed;
-		}
-
-		verticalVelocity += gravity * Time.deltaTime;
-
-		Vector3 speed = new Vector3 (sideSpeed, verticalVelocity, forwardSpeed);
-		speed = transform.rotation * speed;
-
-		characterController.Move (speed * Time.deltaTime);
+	public void MoveCharacter (CharacterController characterController, Vector3 speed) {
+		characterController.Move (speed);
 	}
 }
