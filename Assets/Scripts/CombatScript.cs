e2e62d5acee40ea2071a7590203c9d4a5b2d1eaa
jhavens <thejrh@ymail.com>
2016-02-09 19:35:18 -0600

    added projectileScript and CombatScript

---
+++ Assets/Scripts/CombatScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatScript : MonoBehaviour {
+    public GameObject shot;
+    Transform shotSpawn;
+    private float nextFire;
+    public float fireRate;
+    PlayerScript player;
+
+    // Use this for initialization
+
+    void Start () {
+        shotSpawn = gameObject.transform.GetChild(0);
+        player = gameObject.GetComponent<PlayerScript>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire) //PC control
+        //if (Input.GetButtonDown("Fire1") && Time.time > nextFire && player.numQuarks > 0) //PC control
+        {
+            nextFire = Time.time + fireRate;
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        player.numQuarks--;
+        Instantiate(shot, shotSpawn.position, Camera.main.transform.rotation);
+    }
+}
