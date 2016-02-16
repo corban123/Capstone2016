df9973bb35c72fcdcfc9fc544b34ff5933024ef8
Paige Hinkle <paigehinkle@utexas.edu>
2016-02-16 16:08:27 -0600

    Networked boards yay

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -8,13 +8,29 @@ using System.Linq;
  * Class for sending board information over the network.
  */
 public class BoardManager : NetworkBehaviour {
-	private bool IS_DEBUG = true;
-
 	BoardGenerator generator;
+	List<int[,]> boards;
+	bool found;
 
 	void Start () {
 		// Generate the boards
 		generator = GetComponent<BoardGenerator> ();
-		List<int[,]> boards = generator.CreateBoards();
+		boards = generator.CreateBoards();
+		found = false;
+	}
+
+	// Search for players and set bingo boards
+	void Update () {
+		if (!found) {
+			BoardScript[] boardObjects = FindObjectsOfType (typeof(BoardScript)) as BoardScript[];
+
+			if (boardObjects.Length == 1) {
+				boardObjects [0].SetBoard (boards [0]);
+			} else if (boardObjects.Length == 2) {
+				boardObjects [0].SetBoard (boards [0]);
+				boardObjects [1].SetBoard (boards [1]);
+				found = true;
+			}
+		}
 	}
 }
