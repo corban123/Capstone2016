e93e8963e5fc3509d4c9e03df983cdc711b94a09
Paige Hinkle <paigehinkle@utexas.edu>
2016-03-08 18:48:20 -0600

    fixed the bug

--- Assets/Scripts/StartUpPlayerScript.cs
+++ Assets/Scripts/StartUpPlayerScript.cs
@@ -11,7 +11,6 @@ public class StartUpPlayerScript : NetworkBehaviour {
 	BoardGenerator generator;
 	List<int[,]> boards;
 	bool found;
-    bool foundp1;
 
 	void Start () {
 		// Generate the boards
@@ -24,15 +23,10 @@ public class StartUpPlayerScript : NetworkBehaviour {
 	void Update () {
 		if (!found) {
 			BoardScript[] boardObjects = FindObjectsOfType (typeof(BoardScript)) as BoardScript[];
-			if (boardObjects.Length == 1 && !foundp1) {
+			if (boardObjects.Length == 1) {
 				boardObjects [0].SetBoard (boards [0]);
                 boardObjects [0].gameObject.name = "Player 1";
-                foundp1 = true;
 			} else if (boardObjects.Length == 2) {
-                if (!foundp1) {
-                    boardObjects [0].SetBoard (boards [0]);
-                    boardObjects [0].gameObject.name = "Player 1";
-                }
 				boardObjects [1].SetBoard (boards [1]);
                 boardObjects [1].gameObject.name = "Player 2";
                 found = true;
