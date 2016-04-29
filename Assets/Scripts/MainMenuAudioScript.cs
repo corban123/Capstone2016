e524dbfb9d114e6a58cc35993502667175dcafd2
Paige Hinkle <paigehinkle@utexas.edu>
2016-04-29 14:49:48 -0500

    main menu start up sound

---
+++ Assets/Scripts/MainMenuAudioScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MainMenuAudioScript : MonoBehaviour {
+    public AudioClip startClip;
+    public AudioClip loopClip;
+
+    AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource> ();
+        StartCoroutine(playEngineSound());
+	}
+    IEnumerator playEngineSound()
+    {
+        audio.clip = startClip;
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = loopClip;
+        audio.Play();
+    }
+	
+}
